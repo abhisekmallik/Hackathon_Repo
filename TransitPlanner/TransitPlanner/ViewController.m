1a9ae645b0dee466cb6564e650f7b7f14e764a2f
Abhisek Mallik <abhisek.mallik@outlook.com>
2015-08-20 19:34:43 +0400

    Transit planner
    Initial commit

---
+++ TransitPlanner/TransitPlanner/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TransitPlanner
+//
+//  Created by Abhisek Mallik on 8/20/15.
+//  Copyright © 2015 Emirates. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TransitPlanner/TransitPlanner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TransitPlanner
+//
+//  Created by Abhisek Mallik on 8/20/15.
+//  Copyright © 2015 Emirates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
